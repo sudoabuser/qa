26da298c95428258fe4bc19c09538c9264eb5b38
sudoabuser <kymbl2772@hotmail.com>
2022-11-17 02:01:08 +0300

    = added

--- features/adding_two_integers.feature
+++ features/adding_two_integers.feature
@@ -1,34 +1,35 @@
 Feature: Basic operations
-    We want to use 4 operations: sum, substraction, multiplication and division.
+  We want to use 4 operations: sum, substraction, multiplication and division.
 
-    Scenario: Add two integers
-        Given bob clicked AC
-        And bob clicks <9>
-        And bob clicks plus
-        And bob clicks <5>
-      # When bob clicks =
-        Then bob should see the result <14>
 
-    Scenario: Multiply two integers
-        Given bob clicked AC
-        And bob clicks <7>
-        And bob clicks cross
-        And bob clicks <3>
-      # When bob clicks = 
-        Then bob should see the result <21>
+  Scenario: Add two integers
+    Given bob clicked AC
+    And bob clicks <9>
+    And bob clicks plus
+    And bob clicks <5>
+    When bob clicks =
+    Then bob should see the result <14>
 
-    Scenario: Subtract two integers
-        Given bob clicked AC
-        And bob clicks <6>
-        And bob clicks minus
-        And bob clicks <1>
-      # When bob clicks =
-        Then bob should see the result <5>
+  Scenario: Multiply two integers
+    Given bob clicked AC
+    And bob clicks <7>
+    And bob clicks cross
+    And bob clicks <3>
+    When bob clicks =
+    Then bob should see the result <21>
 
-    Scenario: Divide two integers
-        Given bob clicked AC
-        And bob clicks <8>
-        And bob clicks divide
-        And bob clicks <4>
-      # When bob clicks =  
-        Then bob should see the result <2>
+  Scenario: Subtract two integers
+    Given bob clicked AC
+    And bob clicks <6>
+    And bob clicks minus
+    And bob clicks <1>
+    When bob clicks =
+    Then bob should see the result <5>
+
+  Scenario: Divide two integers
+    Given bob clicked AC
+    And bob clicks <8>
+    And bob clicks divide
+    And bob clicks <4>
+    When bob clicks =
+    Then bob should see the result <2>
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -26,106 +26,89 @@ const opts = {
 
 
 
-    var sumVar = 0;
-    var productVar = 1;
-    var divisionVar = 1;
-    var subtractionVar = 0;
-
-    
-    Given("bob clicked AC", async function () {
-        const driver = await wdio.remote(opts);
-        let attribute = await driver.$('~AC');
-        sumVar = 0;
-        productVar = 1;
-
-        
-        await attribute.click();
-        console.log("AC clicked");
-    });
-
-    When("bob clicks <{int}>", async function (int) {
-        const driver = await wdio.remote(opts);
-        productVar *= int;
-        sumVar += int;
-        divisionVar /= int;
-        subtractionVar -= int;
-        // geldiginde bunu bosverip ekrandaki texti cekmeye calis // // tesekkurler kiyam <3
-
-        let attribute = await driver.$('~' + int);
-        await attribute.click();
-        console.log(int + " clicked");
-    });
+var sumVar = 0;
+var productVar = 1;
+var divisionVar = 1;
+var subtractionVar = 0;
 
 
-    When("bob clicks plus", async function () {
-        const driver = await wdio.remote(opts);
-
-        let attribute = await driver.$('~ + ');
-        await attribute.click();
-        console.log(" + clicked");
-    });
-
-    When("bob clicks divide", async function () {
-        const driver = await wdio.remote(opts);
-
-        let attribute = await driver.$('~ ÷ ');
-        await attribute.click();
-        console.log(" / clicked");
-    });
-
-    When("bob clicks minus", async function () {
-        const driver = await wdio.remote(opts);
+Given("bob clicked AC", async function () {
+    const driver = await wdio.remote(opts);
+    let attribute = await driver.$('~AC');
+    sumVar = 0;
+    productVar = 1;
 
 
-        let attribute = await driver.$('~ - ');
-        await attribute.click();
-        console.log(" - clicked");
-    });
+    await attribute.click();
+    console.log("AC clicked");
+});
 
-    When("bob clicks cross", async function () {
-        const driver = await wdio.remote(opts);
-        let attribute = await driver.$('~ × ');
-        await attribute.click();
-        console.log(" × clicked");
-    })
-    
-    Then("bob should see the result <{int}>", async function (int) {
-        const driver = await wdio.remote(opts);
-        let attribute = await driver.$('~' + int);
-        console.log("sum is: " + sumVar);
+When("bob clicks <{int}>", async function (int) {
+    const driver = await wdio.remote(opts);
+    productVar *= int;
+    sumVar += int;
+    divisionVar /= int;
+    subtractionVar -= int;
+    // geldiginde bunu bosverip ekrandaki texti cekmeye calis // // tesekkurler kiyam <3
 
-        var resultSelector = 'new UiSelector().className("android.view.View").descriptionContains("result")';
-        var resultElement = await driver.$('android=' + resultSelector);
-        var resultContent = await resultElement.getAttribute("content-desc")+ ' ';
-        console.log("result: ", resultContent.slice(7, -1));
-        assert.equal(int, resultContent.slice(7, -1));
-    })
+    let attribute = await driver.$('~' + int);
+    await attribute.click();
+    console.log(int + " clicked");
+});
 
 
+When("bob clicks plus", async function () {
+    const driver = await wdio.remote(opts);
+
+    let attribute = await driver.$('~ + ');
+    await attribute.click();
+    console.log(" + clicked");
+});
+
+When("bob clicks divide", async function () {
+    const driver = await wdio.remote(opts);
+
+    let attribute = await driver.$('~ ÷ ');
+    await attribute.click();
+    console.log(" / clicked");
+});
+
+When("bob clicks minus", async function () {
+    const driver = await wdio.remote(opts);
 
 
+    let attribute = await driver.$('~ - ');
+    await attribute.click();
+    console.log(" - clicked");
+});
 
-    // Then("bob should see the multiplication result <{int}>", async function (int) {
-    //      
-    //     let attribute = await driver.$('~' + int);
-    //     console.log("product is: " + productVar);
+When("bob clicks cross", async function () {
+    const driver = await wdio.remote(opts);
+    let attribute = await driver.$('~ × ');
+    await attribute.click();
+    console.log(" × clicked");
+})
+Then("bob should see the result <{int}>", async function (int) {
+    const driver = await wdio.remote(opts);
+    let attribute = await driver.$('~' + int);
+    console.log("sum is: " + sumVar);
 
-    //     assert.strictEqual(int, productVar);
-    // })
+    var resultSelector = 'new UiSelector().className("android.view.View").descriptionContains("result")';
+    var resultElement = await driver.$('android=' + resultSelector);
+    var resultContent = await resultElement.getAttribute("content-desc") + ' ';
+    console.log("result: ", resultContent.slice(7, -1));
+    assert.equal(int, resultContent.slice(7, -1));
+})
 
-    // Then("bob should see the subtraction result <{int}>", async function (int) {
-    //      
-    //     let attribute = await driver.$('~' + int);
-    //     console.log("substraction is: " + subtractionVar);
+When("bob clicks =", async function () {
+    const driver = await wdio.remote(opts);
+    let attribute = await driver.$('~=');
+    await attribute.click();
 
-    //     assert.strictEqual(int, subtractionVar);
-    // })
+    var equalSelector = 'new UiSelector().className("android.view.View").descriptionContains("=")';
+    var equalElement = await driver.$('android=' + equalSelector);
+    var equalContent = await equalElement.getAttribute("content-desc");
+    assert.strictEqual('=', equalContent);
 
-    // Then("bob should see the division result <{int}>", async function (int) {
-    //      
-    //     let attribute = await driver.$('~' + int);
-    //     console.log("divison is: " + divisionVar);
-
-    //     assert.strictEqual(int, divisionVar);
-    // })
-
+    console.log("= clicked, " + "result added to the history.")
+})
